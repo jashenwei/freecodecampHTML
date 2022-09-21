0c7f9b62b249f643dd39b5357e829d69c625a7c3
JASHEN <oxxxyversus@gmail.com>
2022-09-21 13:57:09 +0900

    for *=

---
+++ 44.js
@@ -0,0 +1,10 @@
+for (var i = 0; i < arr.length; i++) {
+    for (var x = 0; x < arr[i].length; x++) {
+        product *= arr[i][x];
+    }
+}
+
+
+// Only change code above this line
+return product;
+
