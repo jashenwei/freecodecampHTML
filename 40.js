61aa12833311e0a2623f052208ff04a4b79aeb26
JASHEN <oxxxyversus@gmail.com>
2022-09-21 13:25:16 +0900

    iterate old numbers

---
+++ 40.js
@@ -0,0 +1,10 @@
+//Iterate Odd Numbers With a For Loop
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i <= 9; i += 2) {
+    myArray.push(i);
+}
+console.log(myArray)
