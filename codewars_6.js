1df5708ccd2e27f981c999179745af2b7f6fc9c7
JASHEN <oxxxyversus@gmail.com>
2022-09-24 23:54:00 +0900

    days of month

---
+++ codewars_6.js
@@ -0,0 +1,22 @@
+function howManydays(month) {
+    let days = 0;
+    switch (month) {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            days = 31;
+            break;
+
+        case 4: case 6: case 8: case 11:
+            days = 30;
+            break;
+
+        case 2:
+            days = 28;
+            break;
+
+
+    }
+    return days;
+}
+console.log(howManydays(7))
+
+//сколько дней в месяце
